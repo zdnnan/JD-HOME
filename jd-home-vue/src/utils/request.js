681c4b716f5805b60de9436dcecbabf57d2e8a62
zdnnan <3202280364@qq.com>
2023-03-06 16:02:39 +0800

    联调等

--- jd-home-vue/src/utils/request.js
+++ jd-home-vue/src/utils/request.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'https://www.fastmock.site/mock/ae8e9031947a302fed5f92425995aa19/jd',
-  timeout: 10000
+  baseURL: 'http://localhost:3000',
+  timeout: 10000,
+  withCredentials: true
 })
 
 export const get = (url, params = {}) => {
